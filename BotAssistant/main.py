3f5eb07f5349ce9169560813e7f33e91d3284ac9
Danylo <Chornobaidan@gmail.com>
2023-08-26 12:10:05 +0300

    v1.0
    The initial file structure is created

---
+++ BotAssistant/main.py
@@ -0,0 +1,24 @@
+def input_error(func):
+    continue
+
+@input_error
+def add_contact_handler(contacts, name, phone):
+    continue
+
+@input_error
+def change_contact_handler(contacts, name, phone):
+    continue
+
+@input_error
+def phone_handler(contacts, name):
+    continue
+
+@input_error
+def show_all_handler(contacts):
+    continue
+
+def main():
+    continue
+
+if __name__ == "__main__":
+    main()
